f963f1419ccc7aa576600f5c0b5a37e1368df078
farneser <farneser1@gmail.com>
2023-12-21 21:57:34 +0300

    chore: split method

--- split.py
+++ split.py
@@ -1,40 +1,48 @@
 import cv2
 import os
+import time
 
-input_video = "football.mp4"
-output_directory = "frames"
 
-# Создание папок если отсутствуют
-os.makedirs(output_directory, exist_ok=True)
+def split_video(input_video: str = "soccer.mp4", output_directory: str = "frames") -> None:
+    # Создание папок если отсутствуют
+    os.makedirs(output_directory, exist_ok=True)
 
-# открытие видео, как объекта
-cap = cv2.VideoCapture(input_video)
+    # открытие видео, как объекта
+    cap = cv2.VideoCapture(input_video)
 
-if not cap.isOpened():
-    print("Error: Could not open video file.")
-    exit()
+    if not cap.isOpened():
+        print("Error: Could not open video file.")
+        exit()
 
-frame_count = 0
+    frame_count = 0
 
-while True:
-	# чтение кадра
-    ret, frame = cap.read()
+    while True:
+        # чтение кадра
+        ret, frame = cap.read()
 
-	# завершение чтение видео если кадры закончились
-    if not ret:
-        break
+        # завершение чтение видео если кадры закончились
+        if not ret:
+            break
 
-	# построение путя сохранения файла
-	# в фигурных скобрах происходит встраивание переменной `frame_count` в название файла, где `:04d` указывает, что минимальная длинна числа 4 символа "4" -> "0004"
-    frame_filename = os.path.join(output_directory, f"frame_{frame_count:04d}.jpg")
-    # сохранение кадра по пути
-    cv2.imwrite(frame_filename, frame)
+        # построение пути сохранения файла
+        # в фигурных скобках происходит встраивание переменной `frame_count` в название файла,
+        # где `:04d` указывает, что минимальная длинна числа 4 символа "4" -> "0004"
+        frame_filename = os.path.join(output_directory, f"frame_{frame_count:04d}.jpg")
+        # сохранение кадра по пути
+        cv2.imwrite(frame_filename, frame)
 
-    frame_count += 1
+        frame_count += 1
 
-# закрытие потока видео
-cap.release()
+    # закрытие стрима (потока) видео
+    cap.release()
 
-print(f"Extracted {frame_count} frames.")
+    print(f"Extracted {frame_count} frames.")
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print("Splitting video...")
+    split_video(input_video="football.mp4")
+    print(f"Done in {time.time() - start_time} seconds.")
